b877741152e1587b84c4c053d6252be6e9d1886f
Su Wang <suw.mail@gmail.com>
2020-12-15 15:48:29 -0800

    Add generator

---
+++ generate.php
@@ -0,0 +1,36 @@
+<?php
+
+// Load up static content
+$header = file_get_contents('header.html');
+$footer = file_get_contents('footer.html');
+$disclaimer = file_get_contents('disclaimer.html');
+
+/**
+ * Generate a page with given header, footer, and contents
+ *
+ * @param string $header
+ * @param string $footer
+ * @param string $contents
+ *
+ * @return string
+ */
+function generatePage(string $header, string $footer, string $contents)
+{
+  return $header . $contents . $footer;
+}
+
+// Generate index page
+file_put_contents('public/index.html', generatePage($header, $footer, ''));
+
+// Generate climbing pages
+$climbingFiles = scandir('climbing/');
+
+foreach ($climbingFiles as $climbingFile) {
+  if (strpos($climbingFile, '.html') === false) {
+    continue;
+  }
+  $fileContents = $disclaimer . file_get_contents("climbing/$climbingFile");
+  file_put_contents("public/$climbingFile", generatePage($header, $footer, $fileContents));
+}
+
+?>
